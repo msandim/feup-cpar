3d9b8d5e1a4a4312f86e6b6dbaca865be6ad0dca
msandim <migsandim@hotmail.com>
2016-03-09 12:02:31 +0000

    Changes in ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define SYSTEMTIME clock_t
 
  
-void OnMult(int m_ar, int m_br) 
+void OnMult(int m_ar) 
 {
 	
 	SYSTEMTIME Time1, Time2;
@@ -22,7 +22,7 @@ void OnMult(int m_ar, int m_br)
 
 	double *pha, *phb, *phc;
 	
-
+	// Initialize matrix:
     pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
 	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
 	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
@@ -32,32 +32,34 @@ void OnMult(int m_ar, int m_br)
 			pha[i*m_ar + j] = (double)1.0;
 
 
-	for(i=0; i<m_br; i++)
-		for(j=0; j<m_br; j++)
-			phb[i*m_br + j] = (double)(i+1);
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			phb[i*m_ar + j] = (double)(i+1);
 
 
     Time1 = clock();
 
+    // Do the multiplication:
 	for(i=0; i<m_ar; i++)
-	{	for( j=0; j<m_br; j++)
+	{	for( j=0; j<m_ar; j++)
 		{	temp = 0;
 			for( k=0; k<m_ar; k++)
 			{	
-				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+				temp += pha[i*m_ar+k] * phb[k*m_ar+j];
 			}
 			phc[i*m_ar+j]=temp;
 		}
 	}
 
 
+	// Measure the time and give the resulting matrix:
     Time2 = clock();
 	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
 	cout << st;
 
 	cout << "Result matrix: " << endl;
 	for(i=0; i<1; i++)
-	{	for(j=0; j<min(10,m_br); j++)
+	{	for(j=0; j<min(10,m_ar); j++)
 			cout << phc[j] << " ";
 	}
 	cout << endl;
@@ -65,12 +67,10 @@ void OnMult(int m_ar, int m_br)
     free(pha);
     free(phb);
     free(phc);
-	
-	
 }
 
 
-void OnMultLine(int m_ar, int m_br)
+void OnMultLine(int m_ar)
 {
 	SYSTEMTIME Time1, Time2;
 	
@@ -83,7 +83,7 @@ void OnMultLine(int m_ar, int m_br)
 	// Creates matrix a, b and the result matrix c
     pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
 	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
-	phc = (double *)malloc((m_ar * m_br) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
 
 	//Initialize values of matrix a
 	for(i=0; i<m_ar; i++)
@@ -91,23 +91,23 @@ void OnMultLine(int m_ar, int m_br)
 			pha[i*m_ar + j] = (double)1.0;
 
 	//Initialize values of matrix b
-	for(i=0; i<m_br; i++)
-		for(j=0; j<m_br; j++)
-			phb[i*m_br + j] = (double)(i+1);
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			phb[i*m_ar + j] = (double)(i+1);
 
 	//Initialize values of matrix c
 	for(i=0; i<m_ar; i++)
-		for(j=0; j<m_br; j++)
+		for(j=0; j<m_ar; j++)
 			phc[i*m_ar + j] = (double)0.0;
 
 	//Start the clock
 	Time1 = clock();
     
 	for(i=0; i<m_ar; i++)
-	{	for( k=0; k<m_br; k++)
-		{	for( j=0; j<m_br; j++)
+	{	for( k=0; k<m_ar; k++)
+		{	for( j=0; j<m_ar; j++)
 			{	
-				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_ar+j];
 			}
 			
 		}
@@ -124,7 +124,7 @@ void OnMultLine(int m_ar, int m_br)
 	//Print Result matrix (c)
 	cout << "Result matrix: " << endl;
 	for(i=0; i<1; i++)
-	{	for(j=0; j<min(10,m_br); j++)
+	{	for(j=0; j<min(10,m_ar); j++)
 			cout << phc[j] << " ";
 	}
 	cout << endl;
@@ -196,8 +196,7 @@ int main (int argc, char *argv[])
 	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
 	if (ret != PAPI_OK) cout << "ERRO: PAPI_L2_DCM" << endl;
 
-	int n_lin = 600; 
-	int n_col = 600;
+	int matrixSize = 600; 
 	int inc = 400;
 	op = 1;
 	cout << endl << "1. Multiplication" << endl;
@@ -212,16 +211,16 @@ int main (int argc, char *argv[])
 		
 		switch (op){
 			case 1: 
-				OnMult(n_lin, n_col);
+				OnMult(matrixSize);
 				break;
 			case 2:
-				OnMultLine(n_lin, n_col);
+				OnMultLine(matrixSize);
 				break;
 		}
 		
 		ret = PAPI_stop(EventSet, values);
   		if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
-		printf("N LIN: %d\n", n_lin);
+		printf("N LIN: %d\n", matrixSize);
   		printf("L1 DCM: %lld \n",values[0]);
   		printf("L2 DCM: %lld \n",values[1]);
 
@@ -229,9 +228,8 @@ int main (int argc, char *argv[])
 		if ( ret != PAPI_OK )
 			std::cout << "FAIL reset" << endl; 
 		
-		n_lin += inc;
-		n_col = n_lin;
-	} while (n_lin <= 3000 || op == 0);
+		matrixSize += inc;
+	} while (matrixSize <= 3000 || op == 0);
 
 	ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
 	if ( ret != PAPI_OK )
