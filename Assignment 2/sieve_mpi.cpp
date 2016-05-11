c4d1fc757e8bcc40772fb48278a835c9ac36c445
msandim <migsandim@hotmail.com>
2016-05-11 12:24:33 +0100

    Macros

--- Assignment 2/sieve_mpi.cpp
+++ Assignment 2/sieve_mpi.cpp
@@ -4,6 +4,11 @@
 #include <bits/stdc++.h>
 #include "mpi.h"
 
+#define BLOCK_LOW(i,n,p) ((i)*(n)/(p))
+#define BLOCK_HIGH(i,n,p) (BLOCK_LOW((i)+1,n,p)-1)
+#define BLOCK_SIZE(i,n,p) (BLOCK_LOW((i)+1,n,p)-BLOCK_LOW(i,n,p))
+#define BLOCK_OWNER(index,n,p) ((((p)*(index)+1)-1)/(n))
+
 using namespace std;
 
 
