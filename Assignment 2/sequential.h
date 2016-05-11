bce1340d8f6906e98f1a3bd3f63305ba5097accc
jpamorim <josepedropamorim@gmail.com>
2016-05-11 16:25:24 +0100

    sequencial bool array

--- Assignment 2/sequential.h
+++ Assignment 2/sequential.h
@@ -32,6 +32,34 @@ void use_vector_bit(long long n) {
 	Utils::printPrimes(numbers);
 }
 
+void use_bool_array_bit(long long n) {
+	bool * numbers = new bool[n-1];
+	memset(numbers, true, n-1);
+
+	long long k = 2;
+	long long smallest = 3;
+
+	while (k*k <= n) {
+		// Mark all multiples of k between k*k and n:
+		for (long long i = k*k; i <= n; i += k)
+			numbers[i-2] = false;
+
+		// Set k as the smallest urmarked number > k:
+		for(long long i = k+1; i <= n; i++)
+		{
+			if (numbers[i-2]) {
+				smallest = i;
+				break;
+			}
+		}
+		k = smallest;
+	}
+
+	// Plot statistics:
+	cout << "Number of primes: " << Utils::countPrimes(numbers, n-1) << endl;
+	//Utils::printPrimes(numbers, n-1);
+}
+
 void use_unsigned_char(long long n)
 {
 	unsigned char * numbers;
@@ -94,6 +122,9 @@ void sequential(int improvement, long long n)
 			break;
 		case 2:
 			dont_use_smallest(n);
+			break;
+		case 3:
+			use_bool_array_bit(n);
 			break;
 	}
 
